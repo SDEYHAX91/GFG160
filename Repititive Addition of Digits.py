3cba7fc329c1c95a8d5d21745ef4b0ce3f066460
SDEYHAX91 <sourjya.dey2004@gmail.com>
2025-02-09 01:15:02 +0530

    Delete Repititive Addition of Digits.py

--- Repititive Addition of Digits.py
+++
@@ -1,9 +0,0 @@
-#Rule: if a number is divisibe by 9 then, sum of its digits is also divisible by 9, and vice versa.
-def RepAddDigits(n):
-  if n == 0: #if input is 0 return 0
-    return 0
-
-if n % 9 == 0: #in case remainder comes 9 only, then return 9
-  return 9
-
-return n % 9 #returns sum of sum of digits (shall be single digit)
