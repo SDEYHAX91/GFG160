90eef0d178d4ff0a9a2c310bf0f5f57bf45d87d3
SDEYHAX91 <sourjya.dey2004@gmail.com>
2025-02-09 01:01:59 +0530

    Create Reversing array.py

---
+++ Array/Reversing array.py
@@ -0,0 +1,28 @@
+#OPTIMAL CODE TO REVERSE AN ARRAY (NO INBUILT FUNCTIONS OR ARR[::-1] REQUIRED)
+#THERE ARE 2 OPTIMAL CODES
+
+# CODE 1: USING 2 POINTERS IN ONE ITERATION (bOTH MOVING TOWARDS EACH OTHER)
+
+def ReverseArray(arr):
+    left = 0 
+    right = len(arr) - 1
+    
+    while(left < right):
+        arr[left],arr[right] = arr[right],arr[left]
+        left += 1
+        right -= 1
+        
+    return arr
+
+# CODE 2: USING ONE POINTER ONLY IN ONE ITERATION
+
+def ReverseArray(arr):
+    i = 0
+    n = len(arr)
+    for i in range(n//2):
+       arr[i], arr[n-i-1] = arr[n-i-1], arr[i]
+       i += 1
+
+    return arr 
+
+#PEACE
