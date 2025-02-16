60cfbb6ba910debfd3eb20115291b5280bd08805
SDEYHAX91 <sourjya.dey2004@gmail.com>
2025-02-16 13:12:43 +0530

    Update Rotate Array towards left.py

--- Array/Rotate Array towards left.py
+++ Array/Rotate Array towards left.py
@@ -1,18 +1,11 @@
-#OPTIMAL CODE TO ROTATE ARRAY ELEMENTS ANTI-CLOCKWISE BY REVERSAL METHOD
+#MOST OPTIMAL CODE TO ROTATE ARRAY ELEMENTS ANTI-CLOCKWISE BY REVERSAL METHOD
 
-def RotateArrLeft(arr, d):
-    
+def RotateArrLeft(arr, k):
     n = len(arr)
-    d %= n
+    if k < 0: #Ensure that k must be +ve
+        k = abs(k)
+    k %= n #Ensure that k must be within bounds of the array i.e k <= n
     
-    reverseArr(arr, 0, d-1)
-    reverseArr(arr, d, n-1)
-    reverseArr(arr, 0, n-1)
-    
-    return arr
-
-def reverseArr(arr, start, end):
-    while(start < end):
-        arr[start], arr[end] = arr[end], arr[start]
-        start += 1
-        end -= 1
+    back = arr[:k] 
+    front = arr[k:]
+    arr[:] = front + back
